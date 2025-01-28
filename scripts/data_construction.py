2ab4194af2da03f3b226b1bbb3fc41140332d7f5
Mahdi <jaouadi.mahdi1@gmail.com>
2025-01-28 09:16:52 +0100

    test

--- scripts/data_construction.py
+++ scripts/data_construction.py
@@ -9,5 +9,7 @@ def data_construction_func():
     daily_data=yf.download(tickers='TSLA',interval='1d',start='2024-01-01')
     daily_data.rename(columns={'Open':'open','High':'high','Low':'low','Close':'close'},inplace=True)
     daily_data.reset_index(inplace=True,drop=False)
+    daily_data=daily_data.to_numpy()
+    daily_data=daily_data.tolist()
     daily_data=data_cleaning(daily_data)
     return daily_data
