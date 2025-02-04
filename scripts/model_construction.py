78f4837fd32492296e9a8bc1eaa2a7adf1a4b5e1
Mahdi <jaouadi.mahdi1@gmail.com>
2025-02-04 07:20:04 +0100

    test

--- scripts/model_construction.py
+++ scripts/model_construction.py
@@ -5,6 +5,11 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import os
 
+
+
+load_dotenv()
+x=os.getenv('y')
+print(x)
 gemini_llm=ChatGoogleGenerativeAI(model='gemini-1.5-flash-8b')
 response=gemini_llm.invoke('hello')
 print(response.content)
